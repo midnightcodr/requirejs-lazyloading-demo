51ab0b14c6767e51bb95095b54884e6ea6c28d62
Rico Chen <ricochen27@gmail.com>
2013-07-05 13:17:03 -0400

    not lazy loading version

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-/*
- main -> purchase -> credits
-                  -> products
- *
- *
- */
-require(["purchase"],function(purchase){
-  purchase.purchaseProduct();
+define(['util/featurex', 'util/featurey'], function(fx, fy) {
+	window.app={
+		call_x: fx.say,
+		call_y: fy.add
+	};
 });
---
+++ js/util/featurex.js
@@ -0,0 +1,7 @@
+define( function() {
+	return {
+		say: function(msg) {
+			console.log('Look ma I can say '+msg);
+		}
+	}
+} );
